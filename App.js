50238db6c22cbb527ef432387b4fe9cb91f3e223
sadegh <akbarisadegh@gmail.com>
2022-11-07 00:45:58 +0330

    s

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>sadegh</Text>
     </View>
   );
 }
