1d0ce41bcc7197dc9a8542a4ed7ba7bf4e1afced
sadegh <akbarisadegh@gmail.com>
2022-11-07 23:06:20 +0330

    added navigation

--- App.js
+++ App.js
@@ -3,13 +3,33 @@ import { store } from './store'
 import { Provider } from 'react-redux'
 import HomeScreen from './screens/HomeScreen';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import MapScreen from './screens/MapScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <Provider store={store}>
-      <SafeAreaProvider>
-        <HomeScreen />
-      </SafeAreaProvider>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <Stack.Navigator>
+            <Stack.Screen
+             name="HomeScreen" 
+             component={HomeScreen}
+             options={{
+              headerShown: false,
+             }} />
+            <Stack.Screen 
+            name="MapScreen" 
+            component={MapScreen}
+            options={{
+              headerShown: false,
+            }} />
+          </Stack.Navigator>
+        </SafeAreaProvider>
+      </NavigationContainer>
     </Provider>
 
   );
--- components/NavOptions.js
+++ components/NavOptions.js
@@ -2,6 +2,7 @@ import { StyleSheet, Text, View, FlatList, Image, TouchableOpacity } from 'react
 import React from 'react'
 import tw from 'twrnc'
 import { Icon } from '@rneui/themed';
+import { useNavigation } from '@react-navigation/native';
 
 const data = [
      {
@@ -9,7 +10,7 @@ const data = [
           title: "Get a ride",
           image: require('../assets/car.png'),
           screen: "MapScreen",
-     },{
+     }, {
           id: "5678",
           title: "Order food",
           image: require('../assets/food.png'),
@@ -18,27 +19,30 @@ const data = [
 ]
 
 const NavOptions = () => {
-  return (
-    <FlatList
-       data={data}
-       horizontal
-       renderItem={({ item }) => (
-          <TouchableOpacity style={tw`bg-gray-200 p-5 m-1 w-40`}>
-               <View style={tw`text-center flex items-center rounded`}>
-                    <Image 
-                       style={{ width: 100, height: 100, resizeMode: 'contain' }}
-                       source={item.image }
-                    />
-                    <Text style={tw`font-bold text-lg mt-1`}>{item.title}</Text>
-                    <Icon 
-                    style={tw`p-3 bg-black rounded-full mt-3`}
-                    size={20}
-                    name="arrowright" color="white" type="antdesign"/>
-               </View>
-          </TouchableOpacity>
-       )}
-    />
-  )
+     const navigation = useNavigation();
+     return (
+          <FlatList
+               data={data}
+               horizontal
+               renderItem={({ item }) => (
+                    <TouchableOpacity
+                    onPress={() => navigation.navigate(item.screen)}
+                    style={tw`bg-gray-200 p-5 m-1 w-40`}>
+                         <View style={tw`text-center flex items-center rounded`}>
+                              <Image
+                                   style={{ width: 100, height: 100, resizeMode: 'contain' }}
+                                   source={item.image}
+                              />
+                              <Text style={tw`font-bold text-lg mt-1`}>{item.title}</Text>
+                              <Icon
+                                   style={tw`p-2 bg-black rounded-full mt-3`}
+                                   size={20}
+                                   name="arrowright" color="white" type="antdesign" />
+                         </View>
+                    </TouchableOpacity>
+               )}
+          />
+     )
 }
 
 export default NavOptions
